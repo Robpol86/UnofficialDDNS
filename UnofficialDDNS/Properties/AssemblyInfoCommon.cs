bfd295be68e0374520bb3f501036371fc37ed005
robpol86 <robpol86@robpol86.com>
2013-06-05 23:22:28 -0500

    Common Assembly for C#
    Implemented a common AssemblyInfo for UnofficialDDNS and DDNSQuery, to
    centralize versions and descriptions.

--- UnofficialDDNS/Properties/AssemblyInfoCommon.cs
+++ UnofficialDDNS/Properties/AssemblyInfoCommon.cs
@@ -5,15 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "UDDNSQuery DLL" )]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible( false )]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid( "459da938-61ff-4159-9832-2198d1b7db20" )]
+[assembly: AssemblyDescription( "A dynamic DNS service for regular registrars without native DDNS options." )]
+[assembly: AssemblyCompany( "Robpol86" )]
+[assembly: AssemblyProduct( "UnofficialDDNS" )]
+[assembly: AssemblyCopyright( "Copyright © 2013" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -25,4 +20,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion( "1.0.0" )]
+[assembly: AssemblyVersion( "1.0.0" )]
